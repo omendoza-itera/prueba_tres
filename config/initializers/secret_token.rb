a86e0d019060136aaf476958becac355913b3fc4
Oscar Itera <menville.geo@yahoo.com>
2011-12-19 11:37:09 -0600

    subid

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PruebaTres::Application.config.secret_token = '84a655b3f817aba3a2cad5ea04368decd33eb41e62d595cbc4adafd54b83efe60907537cfc7a1da2bad26c4ce0b420dee274f5dd9d51da883c0b740b12829163'
